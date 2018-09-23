6e0f308ac776f41cad0c473b47019680fd2723cf
Guilherme <ferreiraga@outlook.com>
2018-09-23 17:42:06 -0300

    scripts

--- conteudoscompartilhados/slider.js
+++ conteudoscompartilhados/slider.js
@@ -6,29 +6,22 @@
 
 		console.log('setting setSlider to ', selector);
 
-		jQuery(selector).on("slide.bs.carousel", function (e) {
-
-			var $e = jQuery(e.relatedTarget);
-			var idx = $e.index();
-			var itemsPerSlide = 3;
-			var totalItems = jQuery(".carousel-item").length;
-
-			if (idx >= totalItems - (itemsPerSlide - 1)) {
-				var it = itemsPerSlide - (totalItems - idx);
-				for (var i = 0; i < it; i++) {
-					// append slides to end
-					if (e.direction == "left") {
-						jQuery(".carousel-item")
-							.eq(i)
-							.appendTo(".carousel-inner");
-					} else {
-						jQuery(".carousel-item")
-							.eq(0)
-							.appendTo(jQuery(this).find(".carousel-inner"));
-					}
+		$(selector).owlCarousel({
+			loop:true,
+			margin:10,
+			nav:true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:3
+				},
+				1000:{
+					items:5
 				}
 			}
-		});
+		})
 
 	}
 
