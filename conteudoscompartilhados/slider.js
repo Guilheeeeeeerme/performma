b61271c6d441ae70163f8ecc812110ca620a11a7
Guilherme <ferreiraga@outlook.com>
2018-09-23 14:11:07 -0300

    scripts

---
+++ conteudoscompartilhados/slider.js
@@ -0,0 +1,30 @@
+
+window.onload = function () {
+
+	console.log('Performma Card Slider');
+
+	jQuery("#myCarousel").on("slide.bs.carousel", function (e) {
+
+		var $e = jQuery(e.relatedTarget);
+		var idx = $e.index();
+		var itemsPerSlide = 3;
+		var totalItems = jQuery(".carousel-item").length;
+
+		if (idx >= totalItems - (itemsPerSlide - 1)) {
+			var it = itemsPerSlide - (totalItems - idx);
+			for (var i = 0; i < it; i++) {
+				// append slides to end
+				if (e.direction == "left") {
+					jQuery(".carousel-item")
+						.eq(i)
+						.appendTo(".carousel-inner");
+				} else {
+					jQuery(".carousel-item")
+						.eq(0)
+						.appendTo(jQuery(this).find(".carousel-inner"));
+				}
+			}
+		}
+	});
+
+};
---
+++ conteudoscompartilhados/translate-form.js
@@ -0,0 +1,25 @@
+
+window.onload = function () {
+
+	console.log('Performma Translate Login Form');
+
+	try {
+		document.getElementById('um-submit-btn').value = 'Registrar';
+	} catch (e) {
+	}
+	try {
+		document.querySelectorAll('.um-button.um-alt')[0].innerText = 'Entrar';
+	} catch (e) {
+	}
+
+	try {
+		document.querySelectorAll('#loginform > p.login-username > label')[0].innerText = 'E-mail';
+	} catch (e) {
+	}
+
+	try {
+		document.querySelectorAll('.um-field-label > label')[3].innerText = "Confirma Senha";
+	} catch (e) {
+	}
+
+};
