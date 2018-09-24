427af03d93e10c8d99ae5d775d2603760704f89f
Guilherme <ferreiraga@outlook.com>
2018-09-24 19:24:11 -0300

    show

--- conteudoscompartilhados/slider.js
+++ conteudoscompartilhados/slider.js
@@ -2,9 +2,9 @@
 jQuery(document).ready(function () {
 	jQuery('.owl-carousel').owlCarousel({
 		margin: 30,
-		autoplay: true,
-		nav: true,
 		smartSpeed: 900,
+		nav:true,
+		responsiveClass:true,
 		navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
 		responsive : {
 			0 : {
