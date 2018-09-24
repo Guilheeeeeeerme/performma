1719ab09f57afe9db97c880313388a6edee4871a
Guilherme <ferreiraga@outlook.com>
2018-09-24 19:15:48 -0300

    show

--- conteudoscompartilhados/slider.js
+++ conteudoscompartilhados/slider.js
@@ -6,6 +6,17 @@ jQuery(document).ready(function () {
 		autoplay: true,
 		nav: true,
 		smartSpeed: 900,
-		navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"]
+		navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+		responsive : {
+			0 : {
+				items: 1,
+			},
+			480 : {
+				items: 2,
+			},
+			768 : {
+				items: 5,
+			}
+		}
 	});
 });
